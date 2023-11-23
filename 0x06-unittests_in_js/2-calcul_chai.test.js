51b873d0c0473a7e759eeb7d4d238bde4669940e
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-11-23 00:17:50 +0000

    Complete task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,27 @@
+// 2-calcul_chai.test.js
+const chai = require('chai');
+const { expect } = chai;
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  describe('type == "SUM"', () => {
+    it('equal positive numbers', () => {
+      expect(calculateNumber('SUM', 2.0, 2.0)).to.equal(4);
+    });
+
+  });
+
+  describe('type == "SUBTRACT"', () => {
+    it('equal positive numbers', () => {
+      expect(calculateNumber('SUBTRACT', 2.0, 2.0)).to.equal(0);
+    });
+
+  });
+
+  describe('type == "DIVIDE"', () => {
+    it('positive numbers', () => {
+      expect(calculateNumber('DIVIDE', 8.0, 2.0)).to.equal(4.0);
+    });
+  });
+});
+
